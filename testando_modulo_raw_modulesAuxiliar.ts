12dc164699e0df464d99a0f98541f184dc41d234
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-16 14:44:45 -0300

    feat: testando raw de module Auxiliar

---
+++ testando_modulo_raw_modulesAuxiliar.ts
@@ -0,0 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
+import {mainReleaseModulesAuxiliar  } from "https://raw.githubusercontent.com/rzjprogramador/modules_typescript/release_modulesAuxiliar/main.ts"
+
+mainReleaseModulesAuxiliar.inputSleep(10, 4000).then((d: any) => console.log(d))
