5c593485b500a7f121503f99f0568bdbe730fe2b
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-19 10:17:45 -0300

    docs: significados orderFunctions

---
+++ fundamentos_js_ts/_conceitos/significados.ts
@@ -0,0 +1,10 @@
+[
+  {
+    titulo: "orderFunction",
+    traducao: "funcao de primeira ordem",
+    contexto:
+      "é uma funcao anonima  com o paramFuncao e na consequencia invocamos esta funcao",
+    casosDeUso:
+      "OF : Orderfunction, :: apartir do es6 do js usamos em funcoes de array - map(OF), filter(OF), reduce(OF), promise - new Promise( OF ) ,",
+  },
+];
