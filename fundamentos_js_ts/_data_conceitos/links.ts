2e9d65d0472aa1875c67db13e69a56862748ebcb
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-22 10:21:40 -0300

    links

---
+++ fundamentos_js_ts/_data_conceitos/links.ts
@@ -0,0 +1,19 @@
+[
+  {
+    nome: "",
+    categoria: "",
+    links: [""],
+  },
+  {
+    nome: "propostas javascript :: proposal js :: TC39",
+    categoria: "ecma",
+    links: ["https://tc39.es/"],
+  },
+  {
+    nome: "date javascript",
+    categoria: "date",
+    links: [
+      "https://www.treinaweb.com.br/blog/trabalhando-com-data-no-javascript?gclid=CjwKCAjwpayjBhAnEiwA-7ena3RCbLyzb2W7mcEB22mGwxg5ixtNDXafewQg11iZZ0ZbckuAuAROChoChK4QAvD_BwE",
+    ],
+  },
+];
