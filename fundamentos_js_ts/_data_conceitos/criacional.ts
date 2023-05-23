b258e1db8a236056fce276e6ceb5d4059721c3ae
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-23 15:03:33 -0300

    criar objeto via classe com metodo statico  Poo passando objeto

--- fundamentos_js_ts/_data_conceitos/criacional.ts
+++ fundamentos_js_ts/_data_conceitos/criacional.ts
@@ -78,6 +78,31 @@
 
 * metodos_para_todas_instancias: crie como public ou coloque nenhuma marcacao no metodo que sera publico e todas instancias tera este metodo podendo acessar qualquer campo que o objeto tenha
 `,
-    exemplo: ``,
+    exemplo: `
+    type ArgsObjeto = { campo1: string, campo2: string }
+
+class Entidade {
+  constructor(
+    public readonly args: ArgsObjeto,
+  ) {}
+
+  static createEntidade({ campo1, campo2 }: ArgsObjeto) {
+    const newObjeto = { campo1, campo2 }
+    return new Entidade(newObjeto);
+  }
+
+  public nomeCompleto() {
+    return \`$\{this.args.campo1} $\{this.args.campo2}\`;
+  }
+}
+
+const makeEntidade = Entidade;
+const instancia1 = makeEntidade.createEntidade({campo1: "reinaldo", campo2: "zacharias"});
+
+// CONSOLES
+console.log("NOME COMPLETO >>>>>  ", instancia1.nomeCompleto());
+console.log(instancia1);
+export default 1;
+`,
   },
 ];
