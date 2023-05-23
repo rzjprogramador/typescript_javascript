43fe08fe12515a22d9ac261eab67aaf4488420f8
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-23 14:42:25 -0300

    docs: criar objeto em classe statica

---
+++ fundamentos_js_ts/_data_conceitos/criacional.ts
@@ -0,0 +1,83 @@
+[
+  {
+    titulo: "",
+    categoria: ["", ""],
+    linguagem: "",
+    estrategia: "",
+    passos_logicos: ``,
+    exemplo: ``,
+  },
+  {
+    titulo: "criando entidade com primitivos via metodo statico da classe",
+    categoria: ["POO", "criacional", "statico"],
+    linguagem: "",
+    estrategia: "",
+    passos_logicos: `
+    # criando entidade com primitivos via metodo statico da classe:
+
+* definir_campos_pertencentes_ao_objeto: no construtor, nao precisa inicializar seus valor.
+
+* configuracao_campos_do_objeto: {
+  imutavel_apos_primeira_atribuicao: use o modificador <readonly> <somenteLeitura>,
+
+  deixar_prop_inacessivel_fora_do_objetoClasse: use o a visibilidade <private>
+
+  deixar_prop_acessivel_fora_do_objetoClasse: use o a visibilidade <public>
+
+* conceito_estatico: para usar fora somente chamando pela classe.
+
+* criando_metodo_apartir_de_campos_pertencentes_ao_objeto: crie o metodo static, com parametros iguais aos campos pertencentes do objeto, e retorne uma nova instancia da classeObjeto com os aprametros recebidos.
+
+* metodos_para_todas_instancias: crie como public ou coloque nenhuma marcacao no metodo que sera publico e todas instancias tera este metodo podendo acessar qualquer campo que o objeto tenha
+`,
+    exemplo: `
+    class Entidade {
+      constructor(
+        public readonly campo1: string,
+        public readonly campo2: string,
+      ) {}
+
+      static createEntidade(campo1: string, campo2: string) {
+        return new Entidade(campo1, campo2);
+      }
+
+      public nomeCompleto() {
+        return this.campo1 +" "+ this.campo2;
+      }
+    }
+
+    const makeEntidade = Entidade;
+    const instancia1 = makeEntidade.createEntidade("reinaldo", "zacharias");
+
+    // CONSOLES
+    console.log("NOME COMPLETO >>>>>  ", instancia1.nomeCompleto());
+    console.log(instancia1);
+    export default 1;
+    `,
+  },
+  {
+    titulo: "criando entidade com OBJETO via metodo statico da classe",
+    categoria: ["POO", "criacional", "statico"],
+    linguagem: "",
+    estrategia: "",
+    passos_logicos: `
+    # criando entidade com objeto via metodo statico da classe:
+
+* definir_campos_pertencentes_ao_objeto: no construtor, nao precisa inicializar seus valor.
+
+* configuracao_campos_do_objeto: {
+  imutavel_apos_primeira_atribuicao: use o modificador <readonly> <somenteLeitura>,
+
+  deixar_prop_inacessivel_fora_do_objetoClasse: use o a visibilidade <private>
+
+  deixar_prop_acessivel_fora_do_objetoClasse: use o a visibilidade <public>
+
+* conceito_estatico: para usar fora somente chamando pela classe.
+
+* criando_metodo_apartir_de_campos_pertencentes_ao_objeto: crie o metodo static, com parametros iguais aos campos pertencentes do objeto, e retorne uma nova instancia da classeObjeto com os aprametros recebidos.
+
+* metodos_para_todas_instancias: crie como public ou coloque nenhuma marcacao no metodo que sera publico e todas instancias tera este metodo podendo acessar qualquer campo que o objeto tenha
+`,
+    exemplo: ``,
+  },
+];
