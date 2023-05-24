07bde6bc8901e0717cf70da50f5b21acc7edb939
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-24 13:40:46 -0300

    docs: formatador de codigo prettier vscode

--- fundamentos_js_ts/_data_conceitos/formatador_vscode_prettier.ts
+++ fundamentos_js_ts/_data_conceitos/formatador_vscode_prettier.ts
@@ -1,5 +1,7 @@
 /*
 
+importante: para a formatacaoa contecer mais rapida insira um arquivo .prettierrc em cada projeto deno ts
+
 extensao: nao esquecer de ver se a extensao prettier esta corretamente instalada.
 
 prettier: pageExtensaoVscode: https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
@@ -22,5 +24,18 @@ config_no_prettierrc_json: `
   "singleQuote": true,
   "printWidth": 120
 }
+
+para formatacao global melhor no settings json colocar as config do prettier dentro do escopo da linguagem o edior obedece mais rapido a config. senao ele procura o default dalinguagem como por exemplo o que nao é desejado >>
+
+significados_props_de_configuracao:
+virgula_a_direita_ultima_prop_de_objetos: "trailingComma": "none",
+espacos_recuo_tab:  "tabWidth": 2,
+ponto_e_virgula_sentencas:  "semi": false,
+aspas_duplas_em_textos:  "singleQuote": true,
+tamanho_habil_escrita_na_tela:  "printWidth": 130
+
+verificar_no_json:
+  propriedades de config typescript :: "typescript.preferences.quoteStyle": "single", // removi este.
+
 `
 */
---
+++ fundamentos_js_ts/_data_conceitos/modulosImportExport.ts
@@ -0,0 +1,9 @@
+[
+  {
+    titulo: "importar e exportar ao emsmo tempo",
+    linguagem: "typescript",
+    categoria: ["importacao_exportacao", "dependencias"],
+    conceito: "com export { modulo } from \"caminho/\" ele ja faz a importacao e exportacao",
+    exemplo: "export { modulo } from \"caminho/\"",
+  },
+]
