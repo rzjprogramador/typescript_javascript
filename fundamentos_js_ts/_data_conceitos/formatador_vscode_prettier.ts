f371d5ea1bb0340b1e8674b08cbd2c8b7ad1a7b9
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-24 12:04:03 -0300

    okok

---
+++ fundamentos_js_ts/_data_conceitos/formatador_vscode_prettier.ts
@@ -0,0 +1,26 @@
+/*
+
+extensao: nao esquecer de ver se a extensao prettier esta corretamente instalada.
+
+prettier: pageExtensaoVscode: https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+
+github_prettier_vscode: https://github.com/prettier/prettier-vscode
+
+no_vscode_settings: puxar o arquivo de config prettierrc >> "prettier.configPath": ".prettierrc.json",
+
+habilitar_no_vscode_json_o_prettier: { "editor.defaultFormatter" : "esbenp.prettier-vscode" , "editor.formatOnSave" : true }
+
+vscode_habilitar_desabilitar_formatacao_deno: 'aem  "editor.defaultFormatter": TROCAR "denoland.vscode-deno" por "esbenp.prettier-vscode",'
+
+formatador_uso_no_vscode: 'ctrl+shif+p escolha formatDocument', tutorial: https://www.alphr.com/use-prettier-vs-code/
+
+config_no_prettierrc_json: `
+{
+  "trailingComma": "none",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "printWidth": 120
+}
+`
+*/
