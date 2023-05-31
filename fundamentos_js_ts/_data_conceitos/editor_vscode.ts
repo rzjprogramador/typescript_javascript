ec0616197b14c50bc713e1e4b13179dadedfe5ae
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-05-31 11:25:58 -0300

    docs: editor vscode - preserveCase em replace alteracoes

---
+++ fundamentos_js_ts/_data_conceitos/editor_vscode.ts
@@ -0,0 +1,5 @@
+// deno-lint-ignore-file
+{
+  substituir_textos_preservando_CaseMaiusculoMinusculo:
+  "ctrl+F - expanda para aparecer o input Replace que recebe a mudança , selecione o primeiro botao [AB] que é o <PreserveCase> verás se passar o mouse... e vai reservar maiusculas e minusculas sem alterar o formato delas.";
+}
