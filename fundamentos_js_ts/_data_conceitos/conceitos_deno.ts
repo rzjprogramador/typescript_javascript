807ab87e4b5d5397b567e99b5a47c689e1c4fc3c
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-01 16:41:33 -0300

    docs: import_maps multiplos

---
+++ fundamentos_js_ts/_data_conceitos/conceitos_deno.ts
@@ -0,0 +1,10 @@
+// deno-lint-ignore-file no-unused-vars
+
+const conceitos_deno = {
+  permissoes_arquivos: "https://deno.com/manual@v1.34.1/basics/permissions",
+};
+
+const issuesDeno = [
+  "problemas e  sugestao importacoes: https://github.com/denoland/deno/issues/2348",
+  "",
+]
---
+++ fundamentos_js_ts/_data_conceitos/imports_deno/import_maps_Conceitos.ts
@@ -0,0 +1,75 @@
+// deno-lint-ignore-file no-unused-vars
+
+const objConceitoImportMaps = {
+  conceito_comando_rodar:
+    "ao inves de argumentar o arquivo , argumento o <parametro /flag>j<com valor default> que é o arquivo de onde ele buscara os imports que é o import_map.json ",
+
+  rodar: {
+    rodar_test: "deno test --import-map import_map.json",
+    rodar_test_autoreload: "deno test --watch --import-map import_map.json",
+    rodar_test_sintaxe:
+      "deno test --watch --import-map <ARQUIVO_ONDE_ESTAO_IMPORTACOES>.json",
+  },
+
+  rodar_com_multiples_imports: {
+    rodar_arquivo:
+      "`deno run -A --watch --import-map=import_map.json src/main.ts`",
+
+    rodar_test: "`deno test -A --watch --import-map=import_map.json`",
+  },
+
+  definindo_import_maps_json: {
+    no_vscode_settings_do_projeto: `
+    // configure apra enxergar o deno.json e o import_map.json
+    {
+      "deno.config": "./deno.json",
+      "deno.importMap": "./import_map.json"
+    }
+    `,
+  },
+
+  definindo_caminhos_mapeados: {
+    variaveis: {
+      de_diretorio:
+        "se o valorEndereco for até um <DIRETÓRIO/> a variavel tem que ter barra no final é #obrigatorio, #eviteErrar",
+
+      de_arquivo:
+        "se o valorEndereco for até um <ARQUIVO.EXT> a variavel NÃO PODE TER BARRA NO SEU FINAL é #obrigatorio, #eviteErrar",
+    },
+
+    conceito_evite_errar:
+      "#eviteErrar se definiu a variavel ex: @/ é arrobaBARRA entao no uso use @/ depois o que definiu no valor endereco....e assim por diante",
+
+    no_import_map_json: `
+      defina uma variavel ex: "@/" e de um endereco até pasta/ com final barra para uso - ai no uso vao chamar a variavel e terao acesso ao que ela tem na pasta no_bloco_scopes:
+      chame uma das variaveis que vc ja definiu no imports e configure extensoes de escopos para ela - o nome da chave nao estara para uso NÃO SERÁ RELEVANTE, mas o VALOR  de endereço de passar pra ela <no uso> te dará acessos.
+    `,
+
+    tutoriais: [
+      "https://youtu.be/BSQYpkA-Tn8",
+      "chatGpt mergeImport_Maps: https://chat.openai.com/c/d4abd6e1-6255-4dd9-98b4-2868c5935fa6",
+    ],
+
+    exemplo: `
+    // exemplo arquivo :: import_map.json
+
+    {
+      "imports": {
+      "@/": "./src/",
+      "@/entidades": "./src/entidades/"
+      },
+      "scopes": {
+        "@/": {
+          "pasta2": "./src/pasta2/",
+          "pasta3": "./src/pasta3/"
+        },
+        "@/entidades": {
+          "entidade1": "./src/entidades/ent_1/",
+          "entidade2": "./src/entidades/ent_2/",
+          "entidade3": "./src/entidades/ent_3/"
+        }
+      }
+    }
+`,
+  },
+};
