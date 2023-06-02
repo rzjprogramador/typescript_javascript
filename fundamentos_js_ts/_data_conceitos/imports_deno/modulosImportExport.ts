27ea1410709cfd239f8f55d39bc714cd1a92a832
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-01 22:56:58 -0300

    docs: conceito importar por escopo typescript

--- fundamentos_js_ts/_data_conceitos/imports_deno/modulosImportExport.ts
+++ fundamentos_js_ts/_data_conceitos/imports_deno/modulosImportExport.ts
@@ -3,7 +3,17 @@
     titulo: "importar e exportar ao mesmo tempo",
     linguagem: "typescript",
     categoria: ["importacao_exportacao", "dependencias"],
-    conceito: "com export { modulo } from \"caminho/\" ele ja faz a importacao e exportacao",
-    exemplo: "export { modulo } from \"caminho/\"",
+    conceito:
+      'com export { modulo } from "caminho/" ele ja faz a importacao e exportacao',
+    exemplo: 'export { modulo } from "caminho/"',
   },
-]
+  {
+    titulo: "importar em escopo ts",
+    linguagem: "typescript",
+    categoria: ["importacao_exportacao", "dependencias"],
+    conceito:
+      "dentro do escopo>> constante e funcoes só precisa do nome, <tipo> precisa da keyword <type>, <mudavel na importacao> a <default> coloca <default as nomeDoArtefato>",
+    exemplo:
+      "export { type HttpResponse, type InfoController, ok, minhaConstante,  default as modFoo}",
+  },
+];
