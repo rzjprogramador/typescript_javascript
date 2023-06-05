c60331dc00e75c9106a2a56a9add4bd73f16b4f8
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-05 15:39:51 -0300

    refactor: removido tudo que era do editor vscode apra a pasta user/vscode do editor

--- fundamentos_js_ts/_DADOS/editor/editor_vscode.ts
+++
@@ -1,5 +0,0 @@
-// deno-lint-ignore-file
-{
-  substituir_textos_preservando_CaseMaiusculoMinusculo:
-  "ctrl+F - expanda para aparecer o input Replace que recebe a mudança , selecione o primeiro botao [AB] que é o <PreserveCase> verás se passar o mouse... e vai reservar maiusculas e minusculas sem alterar o formato delas.";
-}
