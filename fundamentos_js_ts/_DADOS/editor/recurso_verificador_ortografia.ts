377aba76a34ec7549cfec70e5f31fabf9139d5d6
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-05 13:58:41 -0300

    feat: corretor de ortografia vscode

--- fundamentos_js_ts/_DADOS/editor/recurso_verificador_ortografia.ts
+++ fundamentos_js_ts/_DADOS/editor/recurso_verificador_ortografia.ts
@@ -6,8 +6,12 @@ doc: ["https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.co
 configurar_no_settings_do_editor: 'add no settings vscode a linha que aponta para o português BR `"cSpell.language": "en,pt,pt_BR",`',
 erro_que_aparecera: "Unknown word :: tradução: <palavra desconhecida>",
 conceito: "ao identificar palavra com erro -> corrija, ou adicione ao dicionario do projeto_Workspace ou do usuarioGlobal_User_Settings",
-salvar_palavra_no_dicionario_do_usuario_global_user: "no quick fix -> clique em add ao <"user settings"> ",
-salvar_palavra_no_dicionario_do_projeto_workspace: ""no quick fix -> clique em add ao <"workspace settings, obs: ao add no do workspace ele criara uma chave no .vscode/settings do projeto com as palavras adicionadas ao dicionario local. com a chave <"cSpell.words"> que será um array de palavras adicionadas."> ,
+salvar_palavra_no_dicionario_do_usuario_global_user: "no quick fix -> clique em add ao <"user settings">.
+
+obs: ao add no do workspace ele criara uma chave no .vscode/settings do projeto com as palavras adicionadas ao dicionario global. com a chave <"cSpell.userWords"> que será um array de palavras adicionadas
+",
+salvar_palavra_no_dicionario_do_projeto_workspace: ""no quick fix -> clique em add ao <"workspace settings.
+obs: ao add no do workspace ele criara uma chave no .vscode/settings do projeto com as palavras adicionadas ao dicionario local. com a chave <"cSpell.words"> que será um array de palavras adicionadas.">,
 quer_corrigir_manual: "clique no quick fix/consertoRapido e clique no modo correto da palavra em ingles.",
 
 tutoriais: ["dechamps: https://www.youtube.com/watch?v=FCC2GbStmfc&t=406s", ""],
