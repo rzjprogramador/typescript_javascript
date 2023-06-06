3191c1c675cafe20a1f11549f761a2c375b6179c
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-06 09:16:02 -0300

    feat: add convencoes nomes

--- fundamentos_js_ts/_DADOS/convencoes/nomeacoes.ts
+++ fundamentos_js_ts/_DADOS/convencoes/nomeacoes.ts
@@ -1,39 +1,88 @@
-const convencao_nomes = {
-  nomear_lib: "<iminuscoloENTIDADE> exemplos: [ <libRZ>, ]",
-  nomear_interfaces_types:
-    "com <I no inicio>, é explicitamente consagrada que é uma interface",
+// deno-lint-ignore-file no-unused-vars
 
-  nomear_makers_types: {
-    interno: "IEntidade",
-    externo: "IMakerI",
+interface INomeacao {
+  titulo: string;
+  categoriaTipoDePropriedade: string;
+  conceito: string;
+  na_definicao: string;
+  membros: string;
+  ao_replicar: string;
+}
+
+const convencaoNomes: INomeacao[] = [
+  {
+    titulo: "nomear_lib",
+    categoriaTipoDePropriedade: "arquivo_libs",
+    conceito: "",
+    na_definicao: "<minuscoloENTIDADE> exemplos: [ <libRZ>, ]",
+    membros: "#not",
+    ao_replicar: "#not",
   },
-
-  import_map_variavel: ["@entidade", "exemplos: @externals"],
-
-  nomear_artefato_Base: "_baseEntidade",
-
-  nome_artefato_de_feature:
-    "iniciar com _ underline -> para deixar pro uso na replica renomear para nomeOriginal sem _ underline e usar a replica.",
-
-  nomeacoes_Lib: {
-    funcionalidades: {
-      conceito: "",
-      pasta: "feature/ || featFuncionalidade",
-      arquivo: "feature.ts ",
-    },
-
-    links: {
-      conceito:
-        "links é um pré main com todos os links das features, que será puxado pelo main distribuir.  faça mesmo que por enquanto nao precise. #futuro",
-      pasta: "/ <na raiz>",
-      arquivo: "links.ts ",
-    },
-
-    principal: {
-      conceito:
-        "o principal que sempre vai representar lá fora é o main, aqui posso publicar do jeito que vao usar seja em objeto, em repasse de link de funções...vou puxar do links e distribuir a gosto e tipo de uso.",
-      pasta: "/ <na raiz>",
-      arquivo: "main.ts ",
-    },
+  {
+    titulo: "nomear contratos types interfaces",
+    categoriaTipoDePropriedade: "code",
+    conceito: "definir com inicio I de interface, e replicar com inicio X .",
+    na_definicao: "<I> IEntidade",
+    membros: "#not",
+    ao_replicar: "<X> XEntidade, exemplos: [ // INomeacao >> XNomeacao, ]",
   },
-};
+  {
+    titulo: "import_map variável",
+    categoriaTipoDePropriedade: "code",
+    conceito: "",
+    na_definicao: '<@>, exemplos: ["@entidade", "exemplos: @externals", ]',
+    membros: "#not",
+    ao_replicar: "#not",
+  },
+  {
+    titulo: "nomear_artefato_Base e artefato de distribuição lib",
+    categoriaTipoDePropriedade: "code",
+    conceito: "",
+    na_definicao: "<_ underline> _baseEntidade",
+    membros: "#not",
+    ao_replicar: "<remove só o underline do inicio>",
+  },
+  {
+    titulo: "nomeacao lib - funcionalidades",
+    categoriaTipoDePropriedade: "diretorio_lib",
+    conceito: "",
+    na_definicao: "feature/ || featFuncionalidade",
+    membros: "feature.ts",
+    ao_replicar: "#not",
+  },
+  {
+    titulo: "nomeação arquivo link em libs",
+    categoriaTipoDePropriedade: "arquivo_link_lib",
+    conceito:
+      "links é um pré main com todos os links das features, que será puxado pelo main distribuir.  faça mesmo que por enquanto nao precise. #futuro",
+    na_definicao: "links.ts",
+    membros: "#not",
+    ao_replicar: "#not",
+  },
+  {
+    titulo: "nomeação arquivo principal distribuidor lib",
+    categoriaTipoDePropriedade: "arquivo principal distribuidor",
+    conceito:
+      "o principal que sempre vai representar lá fora é o main, aqui posso publicar do jeito que vao usar seja em objeto, em repasse de link de funções...vou puxar do links e distribuir a gosto e tipo de uso.",
+    na_definicao: "main.ts",
+    membros: "#not",
+    ao_replicar: "#not",
+  },
+  {
+    titulo: "",
+    categoriaTipoDePropriedade: "",
+    conceito: "",
+    na_definicao: "",
+    membros: "#not",
+    ao_replicar: "#not",
+  },
+  // modelo:
+  // {
+  //   titulo: "",
+  //   categoriaTipoDePropriedade: "",
+  //   conceito: "",
+  //   na_definicao: "",
+  //   membros: "#not",
+  //   ao_replicar: "#not",
+  // },
+];
