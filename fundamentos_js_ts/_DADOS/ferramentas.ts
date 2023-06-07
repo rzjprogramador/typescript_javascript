493a9098a3a2188733def2647fde07e8d7dea6d7
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-07 15:34:26 -0300

    feat: add ferramenta renomear replica

--- fundamentos_js_ts/_DADOS/ferramentas.ts
+++ fundamentos_js_ts/_DADOS/ferramentas.ts
@@ -1,10 +1,26 @@
 [
   {
-    titulo: "verificador tempo execucao",
+    titulo: "verificador tempo execução",
     ferramenta: "console.time() e console.timeEnd()",
+    linguagens: ["typescript", ""],
     beneficio:
-      "vai devolver o tempo que levou apra executar o codigo que esta no meio destas funcoes.",
+      "vai devolver o tempo que levou para executar o código que esta no meio destas funções.",
     uso:
-      "antes do codigo ```console.time()``` e depois do codigoAlvo ```console.timeEnd()```",
+      "antes do codigo ```console.time()``` e depois do códigoAlvo ```console.timeEnd()```",
+  },
+  {
+    titulo: "renomear replica no momento da reatribuição.",
+    ferramenta: "posso renomear a replica após o as Replica em TS",
+    linguagens: ["typescript", ""],
+    beneficio: "#facilitacao",
+    uso:
+      "```// exemplo: ao selecionar a replica após a keyword as -> clico em  F2 e renomeio.```",
+  },
+  {
+    titulo: "",
+    ferramenta: "",
+    linguagens: ["typescript", ""],
+    beneficio: "#facilitacao",
+    uso: "```// exemplo: ```",
   },
 ];
