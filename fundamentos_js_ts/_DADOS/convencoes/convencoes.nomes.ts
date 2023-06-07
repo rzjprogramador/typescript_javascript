5d94fd7a364701896f1d0f520cc5b8fc6f102339
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-07 14:19:44 -0300

    foo: clean

--- fundamentos_js_ts/_DADOS/convencoes/convencoes.nomes.ts
+++ fundamentos_js_ts/_DADOS/convencoes/convencoes.nomes.ts
@@ -1,17 +1,28 @@
 const convencaoNomes = [
   {
     titulo: "nomear ação verificadora",
-    utilizar: "<has> seguido do que ela vai verificar na sequencia",
+    categorias: ["", ""],
+    conceito: "<has> seguido do que ela vai verificar na sequencia",
     exemplo: [
       "ex: hasSpace, hasLengthTextMinForFirstName temTamanhoDeTextoMinimoParaPrimeiroNome",
     ],
   },
   {
     titulo: "nomear artefato principal lib ou que vai ser replicado",
-    utilizar:
-      "definir com inicio em minuscula e na replica replicar com inicio em minuscula",
+    categorias: ["nomeação", "nome_minha_Lib", "nome_ArtefatoPrincipalReplica"],
+    conceito:
+      "definir em <camelCase> --> e na replica replicar como <PascalCase>",
     exemplo: [
       "{ minhaLib as MinhaLib }",
+    ],
+  },
+  {
+    titulo: "nomear minha Lib e Arterfato Principal",
+    categorias: ["nomeação", "nome_minha_Lib", "nome_ArtefatoPrincipalReplica"],
+    conceito:
+      "definir com a sintaxe -> <Recurso + ALIAS_LINGUAGEM__TUDO_MAIÚSCULO> ex: SuperDateTS ",
+    exemplo: [
+      "SuperDateTS",
     ],
   },
 ];
