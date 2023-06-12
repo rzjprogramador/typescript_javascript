2063de4be3096f1ccb91caf7bd9cb26ad1b8d31f
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-12 14:32:47 -0300

    feat: mudar navegador padrao linux

---
+++ fundamentos_js_ts/_DADOS/linux/conceitos_linux.ts
@@ -0,0 +1,11 @@
+// deno-lint-ignore-file no-unused-vars
+
+const conceitos_linux = [
+  {
+    titulo: "mudar navegador padrao no linux",
+    categoria: "navegador",
+    contexto: "linux ubuntu - debian",
+    conceito: "`sudo update-alternatives --config x-www-browser` --> Digite o número do navegador e confirme.",
+    observacoes: "#null",
+  },
+]
