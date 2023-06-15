5dada71fe10f2434c8f079b6cf7651d7a4339a28
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-15 15:46:32 -0300

    docs: aprendizado retornos :: eite metodo retorna throw

---
+++ fundamentos_js_ts/_DADOS/retornos.ts
@@ -0,0 +1,13 @@
+/*
+APRENDIZADO
+-- Na checagem para pegar o erro no catch depois na checagem
+na origem é obrigatorio lançar um novo erro ex: throw new Error(feedback)
+
+-- evite o method que retorna um throw como badArgsPrimitive - causa: depois da complicações para tipar os retornos ja que se ele retornar vai retornar void e da conflito com o retorno de outros artefatos porque vc tera que colocar este void deste metodo - solução sempre retorne throw new Error generico do js ou new ErrorPersonalizado.
+
+/q exemplo metodo de retorno throw a ser evitado:
+// function badArgsPrimitive(f: string): void {
+//     throw new Error(f)
+// }
+
+*/
