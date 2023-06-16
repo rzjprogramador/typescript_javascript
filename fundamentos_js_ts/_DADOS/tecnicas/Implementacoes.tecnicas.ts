653c1fa662039e3acac1b583cf5d660408123afb
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-16 11:23:51 -0300

    refactor: ajustes retorno tecnicas

--- fundamentos_js_ts/_DADOS/tecnicas/Implementacoes.tecnicas.ts
+++ fundamentos_js_ts/_DADOS/tecnicas/Implementacoes.tecnicas.ts
@@ -1,8 +1,8 @@
 // deno-lint-ignore-file no-unused-vars
 
-const tecnicas_Implementacoes = {
+const implementacoes_tecnicas = {
   titulo: "agrupar artefatos da mesma familia",
-  categoria: "implementacoes",
+  categoria: "implementacoes_tecnicas",
   contexto: "conciso",
   conceito: "conciso: preferir agrupar em um objeto, quando se tem varias variaveis da mesma classificacao. ",
   observacoes: "#null",
---
+++ fundamentos_js_ts/_DADOS/tecnicas/Retorno.tecnicas.ts
@@ -0,0 +1,21 @@
+// deno-lint-ignore-file no-unused-vars
+
+const retorno_tecnicas = [
+  {
+    titulo: "tecnica pegar retorno de promessa tratada com tryCatch para test",
+    categorias: ["retorno_de_promessas", "test"],
+    contexto: "pegar retorno de promessa tratada com tryCatch",
+    conceito: `
+    1. fazer uma funcaoPromessa -> com trycatch que como sucesso -> tenta o retornar a execução da promessa.
+    2. no catch -> pegar a .message do erro
+    3. na suiteTest -> guardar em variavelLink a execucao da funcaoPromessa
+    4. será guardado o que deseja usar da funcaoPromessa no test - desencadear .then() para guardar o sucesso || ou desencadear .catch() para guardar a falha com o feedback ..DAI SÓ TESTAR O GUARDADO.
+    `,
+    observacoes: `
+    // EXEMPLOS
+    const guardaSucesso = funcaoPromessa().then(ok => ok) // terei o ok
+    const guardaFalha = funcaoPromessa().catch(err => err) // terei a falha com o feedback
+    `,
+  },
+]
+
