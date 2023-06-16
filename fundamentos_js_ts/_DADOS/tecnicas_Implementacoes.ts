c8175a470774d9eacebbe58803b67bb068b40efc
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-16 10:45:22 -0300

    feat: tecnica d e implementacoes agrupar artefatos concisos da mesma familia

---
+++ fundamentos_js_ts/_DADOS/tecnicas_Implementacoes.ts
@@ -0,0 +1,9 @@
+// deno-lint-ignore-file no-unused-vars
+
+const tecnicas_Implementacoes = {
+  titulo: "agrupar artefatos da mesma familia",
+  categoria: "implementacoes",
+  contexto: "conciso",
+  conceito: "conciso: preferir agrupar em um objeto, quando se tem varias variaveis da mesma classificacao. ",
+  observacoes: "#null",
+}
