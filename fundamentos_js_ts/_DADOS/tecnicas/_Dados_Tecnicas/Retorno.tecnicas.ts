e98e5872b8ae314355fc535122db55f184945255
@rzjprogramador <60945992+rzjprogramador@users.noreply.github.com>
2023-06-16 18:08:14 -0300

    important: naoEsqueca usar await antes de promessa guardada.

--- fundamentos_js_ts/_DADOS/tecnicas/_Dados_Tecnicas/Retorno.tecnicas.ts
+++ fundamentos_js_ts/_DADOS/tecnicas/_Dados_Tecnicas/Retorno.tecnicas.ts
@@ -9,7 +9,7 @@ const retorno_tecnicas = [
     1. fazer uma funcaoPromessa -> com trycatch que como sucesso -> tenta o retornar a execução da promessa.
     2. no catch -> pegar a .message do erro
     3. na suiteTest -> guardar em variavelLink a execucao da funcaoPromessa
-    4. será guardado o que deseja usar da funcaoPromessa no test - desencadear .then() para guardar o sucesso || ou desencadear .catch() para guardar a falha com o feedback ..DAI SÓ TESTAR O GUARDADO.
+    4. será guardado o que deseja usar da funcaoPromessa no test - desencadear .then() para guardar o sucesso || ou desencadear .catch() para guardar a falha com o feedback ..DAI SÓ TESTAR O GUARDADO. #importante #naoEsqueca ao usar o guardadoLink insira   o await na frete pra esperar a resolução da promessa guardada.
     `,
     observacoes: `
     // EXEMPLOS
