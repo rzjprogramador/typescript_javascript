f295f99499a8dc7eb4beae60947ca8a735f2777d
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-19 15:31:17 -0300

    feat: [linux] comando instalar fontes linux

---
+++ fundamentos_js_ts/_DADOS/linux/comandos.use.linux.ts
@@ -0,0 +1,17 @@
+
+const comandos_use_linux = [
+  {
+    categoria: '',
+    contexto: '',
+    titulo: '',
+    comando: '',
+    observacoes: '',
+  },
+  {
+    categoria: 'comandos_use_linux',
+    contexto: 'instalacao',
+    titulo: 'instalacao de fontes no linux',
+    comando: 'fc-cache -fv',
+    observacoes: 'após copiar para o home/.fonts as fontes desejadas rode o comando para instalar e propag o shell.',
+  },
+]
