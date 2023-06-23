6b84f3d5ad7cddedd743fee6e37d117f645b0a87
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-23 14:30:55 -0300

    refactor: okok

---
+++ editor_ARRUMS/nvimRZ/atalhos.ts
@@ -0,0 +1,44 @@
+const atalhos_nvimRZ = [
+  {
+    categoria: "atalhos_nvim",
+    titulo: "",
+    detalhes: "",
+    conceito: "#null",
+    obs: "#null",
+  },
+  {
+    categoria: "atalhos_nvim",
+    titulo: "mostrar arquivos ocultos no explorer NeoTree",
+    detalhes: "shift+h - no nvim >> <S-h>",
+    conceito: "#null",
+    obs: "#null",
+  },
+  {
+    categoria: "atalhos_nvim",
+    titulo: "mostrar todas opções disponíveis no explorer NeoTree",
+    detalhes: "clique em algum arquivo do explorador e digite <?> interrogação",
+    conceito: "#null",
+    obs: "#null",
+  },
+  {
+    categoria: "atalhos_nvim",
+    titulo: "toggle vsplit terminal",
+    detalhes: "<Esc>tt",
+    conceito: "#null",
+    obs: "#null",
+  },
+  {
+    categoria: "atalhos_nvim",
+    titulo: "float terminal",
+    detalhes: "<S-t>",
+    conceito: "#null",
+    obs: "#null",
+  },
+  {
+    categoria: "atalhos_nvim",
+    titulo: "deletar remover linhas",
+    detalhes: "dd",
+    conceito: "#null",
+    obs: "#null",
+  },
+];
