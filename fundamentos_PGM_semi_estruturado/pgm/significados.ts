40ce0b6623f6c48d1466795dc6f54db1642f5e04
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-27 11:43:39 -0300

    docs: pgm significados

---
+++ fundamentos_PGM_semi_estruturado/pgm/significados.ts
@@ -0,0 +1,46 @@
+// deno-lint-ignore-file no-unused-vars
+const significados = [
+  {
+    categoria: "significados",
+    titulo: "",
+    significados: ["",],
+  },
+  {
+    categoria: "significados",
+    titulo: "lógica",
+    significados: ["formula", "passo a passo", "implementação", "manipular operadores - principalmente lógicos", "gerar um resultado", "verdadeiro ou falso", "o que faz sentido",],
+  },
+  {
+    categoria: "significados",
+    titulo: "get",
+    significados: ["obter", "buscar", "pesquisar", "adquirir",],
+  },
+  {
+    categoria: "significados",
+    titulo: "configurar",
+    significados: ["atribuir", "preencher", "setar", "definir", "tipar", "função", "objeto", "variável", "campo"],
+  },
+  {
+    categoria: "significados",
+    titulo: "remoto",
+    significados: ["online", "nas nuvens"],
+  },
+  {
+    categoria: "significados",
+    titulo: "local",
+    significados: ["no seu hardware ou lugar - local onde está fazendo",],
+  },
+  {
+    categoria: "significados",
+    titulo: "entity",
+    significados: ["entidade", "module", "módulo"],
+  },
+  {
+    categoria: "significados",
+    titulo: "feature",
+    significados: ["funcionalidade", "função", "metodo",],
+  },
+
+]
+
+
