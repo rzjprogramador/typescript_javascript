40854e88c46fdfb278f6d74a605f07b6d1c5fc9a
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-08 14:49:26 -0300

    feat: criacional entidade POOO abstrata

---
+++ _CODE_FUNDAMENTOS_JS_TS/criacional/Poo.Entidade/factory.ts
@@ -0,0 +1,54 @@
+// -- Base --
+
+abstract class BaseEntityPerson<T> {
+  public readonly args: T
+  // protected ID: string
+
+  constructor(args: T, ID: string) {
+    this.args = args
+    // this.ID = ID ?? '1' // TODO INSERIR UM GERADOR DE HASH PARA ID
+  }
+}
+
+// -- Types --
+interface ArgsEntidade {
+  ID?: string
+  texto1: string
+  texto2: string
+  num1: number
+  num2: number
+}
+
+// -- Factory apartir da Base --
+
+class FactoryEntidade extends BaseEntityPerson<ArgsEntidade> {
+  private constructor(args: ArgsEntidade, ID: string) {
+    super(args, ID)
+  }
+
+  static async create(args: ArgsEntidade, ID?: string) {
+    const entity = new FactoryEntidade(args, ID!)
+    return await entity
+  }
+
+  // members
+  metodoInstancias() {
+    return `Ola sou metodo para todas istancias!`;
+  }
+}
+
+// maker
+const makerFactoryEntidade = async (args: ArgsEntidade, ID?: string) => {
+  return await FactoryEntidade.create(args, ID)
+}
+
+// -- requests --
+const request1 = {
+  texto1: 'texto1',
+  texto2: 'texto2',
+  num1: 10,
+  num2: 20,
+}
+
+// -- Uso Entidade --
+makerFactoryEntidade(request1).then((ok: any) => console.log(ok))
