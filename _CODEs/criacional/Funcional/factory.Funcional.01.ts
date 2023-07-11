58e579d7e1f9fc6bb0409d467bce719a6385180c
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-11 10:58:13 -0300

    okok

---
+++ _CODEs/criacional/Funcional/factory.Funcional.01.ts
@@ -0,0 +1,32 @@
+// deno-lint-ignore-file no-unused-vars
+import { membersEntity } from "./members.ts";
+
+interface Args {
+  c1: string
+  c2: string
+}
+
+const membersEntity = {
+
+  membro1(this: Args) {
+    return this.c1
+  },
+
+  membro2(this: Args) {
+    return `${this.c2}`
+  }
+}
+
+const factoryEntity = async (args: Args) => {
+  const factory = await Object.create(membersEntity)
+  return await Object.assign(factory, args)
+}
+
+export default 1
+
+/*
+TODO: COMPUTAR O CAMPO IDADE - SEM CRIAR UM NOVO FORA DO OBJ.
+
+no maker tive que retornar .props que é o empacota as props usadas no construtor.
+e apra poder acessar essa props tive que deixar public a sua configuracao, mas protegida com Readonly somente leitura após primeira atribuição ou atribuição via metodo.
+*/
