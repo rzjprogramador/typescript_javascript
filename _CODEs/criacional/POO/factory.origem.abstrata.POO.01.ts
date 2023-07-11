7802a1d80c915a15d8bf01fa614392adb2aa3fce
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-11 11:20:48 -0300

    feat: entidade baseada em abstrata

--- _CODEs/criacional/POO/factory.origem.abstrata.POO.01.ts
+++ _CODEs/criacional/POO/factory.origem.abstrata.POO.01.ts
@@ -1,12 +1,14 @@
-// -- Base --
+// deno-lint-ignore-file no-unused-vars
 
+// -- Base abstrata para todas entidades --
 abstract class BaseEntityPerson<T> {
   public readonly args: T
-  // protected ID: string
+  protected ID: string
 
   constructor(args: T, ID: string) {
     this.args = args
-    // this.ID = ID ?? '1' // TODO INSERIR UM GERADOR DE HASH PARA ID
+    this.ID = ID ?? Date.now().toString()
+    // TODO INSERIR UM GERADOR DE HASH PARA ID
   }
 }
 
@@ -23,23 +25,25 @@ interface MembersEntidade {
   metodoInstancias?: () => string
 }
 
-// -- Factory apartir da Base_Abstrata --
+// -- Factory baseada na Base_Abstrata --
 
 class FactoryEntidade extends BaseEntityPerson<ArgsEntidade> {
 
   private constructor(args: ArgsEntidade, ID: string) {
     super(args, ID)
+
+    // Computar - sobreescreve até o request da instanciacao/criacao
+    this.args.texto2 = this.metodoInstancias()  // acessando membros via campos
   }
 
   static async create(args: ArgsEntidade, ID?: string) {
-    const entity = new FactoryEntidade(args, ID!)
-    return await entity
-    // todo: se desempacotar o args aqui nao reconhece a computadocao dos membros.
+    return await new FactoryEntidade(args, ID!)
+    // obs: se desempacotar o args aqui nao reconhece a computação dos membros.
   }
 
   // members
   metodoInstancias() {
-    return `Ola sou metodo para todas istancias .. computando ${this.args.texto1} com ${this.args.texto2} computando numeros num1+num2 == >> ${this.args.num1 + this.args.num2} .`;
+    return `Computando ${this.args.texto1} com ${this.args.texto2} somado >> ${this.args.num1 + this.args.num2} .`;
   }
 }
 
@@ -49,24 +53,14 @@ const makerFactoryEntidade = async (args: ArgsEntidade, ID?: string) => {
 }
 
 // -- requests --
-const request1: ArgsEntidade = {
-  texto1: 'texto1',
-  texto2: 'texto2',
-  num1: 10,
-  num2: 20,
-}
-const request2 = {
-  texto1: 'texto_1_Request2',
-  texto2: 'texto_2_Request2',
-  num1: 10,
-  num2: 20,
-}
+const request1: ArgsEntidade = { texto1: 'texto1', texto2: 'texto2', num1: 10, num2: 20, }
+const request2 = { texto1: 'texto_1_Request2', texto2: 'texto_2_Request2', num1: 10, num2: 20, }
 
 // -- Uso Entidade --
 // makerFactoryEntidade(request1).then((ok: any) => console.log(ok))
 
 const instancia1 = await makerFactoryEntidade(request1)
-const instancia2 = await makerFactoryEntidade(request2)
+// const instancia2 = await makerFactoryEntidade(request2)
 
 console.log(instancia1)
 console.log(instancia1?.metodoInstancias?.())
@@ -74,5 +68,13 @@ console.log(instancia1?.metodoInstancias?.())
 // console.log(instancia2)
 // console.log(instancia2.metodoInstancias())
 
-// verificando membros de instancia
-// console.log(instancia1.metodoInstancias === instancia2.metodoInstancias) // R: true OK - aponta pro mesmo lugar na memoria
+// -- Verificando membros de instancia --
+// console.log(instancia1.metodoInstancias === instancia2.metodoInstancias)
+// R: true OK - aponta pro mesmo lugar na memoria
+
+/*
+DETALHES
+instancia_final: "desta forma as instancias finais ficaram com subObjetos inseridos na sua criacao ex: args, não é uma má ideia fica organizado suas props."
+*/
+
+export default 1
---
+++ _fundamentos_PGM_semi_estruturado/valor_ferramentas_sobre_valores/geradores_de_valor.ts
@@ -0,0 +1,9 @@
+[
+  {
+    titulo: "gerador de string baseado em data",
+    categorias: ["gerador_string",],
+    contexto: "ideal para hash de senhas",
+    conceito: "Date.now().toString()",
+    observacoes: "#null",
+  },
+]
