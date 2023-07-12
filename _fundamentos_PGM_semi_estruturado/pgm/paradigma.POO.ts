de5ce98d3dcbb9c000bb8567f6ff5d05a5692fa0
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-12 11:41:37 -0300

    docs: Poo metodos staticos

---
+++ _fundamentos_PGM_semi_estruturado/pgm/paradigma.POO.ts
@@ -0,0 +1,33 @@
+[
+  {
+    titulo: "utilizar classe com metodos estaticos",
+    categorias: ["classe", "metodos_estaticos"],
+    contexto: "use chammando diretamente a classe e seus metodos sem instanciar",
+    conceito: "se vai usar só os metodos eles tem que ser staticos e nao precisa criar construtor, nem instanciar a classe... use a classe diretamente chamando os metodos estaticos.",
+    observacoes: "#null",
+    exemplos: `
+    class SuperTests {
+      // private constructor() { }
+
+      /**
+         * @description  catchMessageThrowNewError : captura e compara mensagem exata do lançamento de novo erro.
+         * @input  methodTargetAsync  returns .toEqual(messageError)
+         * @exemple expect(await getAsyncMessageNewErrorThrowTest ( methodtargetAsync )).toEqual('msg_1')
+         */
+
+      static async catchMessageThrowNewError(methodTarget: any) {
+        try {
+          return await methodTarget()
+        }
+        catch (err: any) {
+          return err.message
+        }
+      }
+
+    }
+
+    export { SuperTests }
+    `,
+  }
+]
+
