b69d2b2cb339d337a7d49cfbc4e9f668fdca3016
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-13 13:46:54 -0300

    feat: uso de classe utilitaria do this, usando tecnica factoryMethod onde a propria classe se instancia

---
+++ _CODEs/_Paradigmas/POO/classes_utiltarias/classe_Utilitaria.UseOThis.Com.FactoryMethod_NaClasse.ts
@@ -0,0 +1,45 @@
+
+class Entity {
+  private campo1!: number
+  private campo2!: number
+
+  constructor() {
+    this.campo1 = 10
+    this.campo2 = 20
+  }
+
+  static _preMaker() {
+    return new Entity()
+  }
+
+  async perform() {
+    return await `>>>>>>>>>> Hello Word >> Comput Soma C1+C2 === ${this.campo1 + this.campo2}`
+  }
+}
+
+const makerEntity = await Entity._preMaker()
+
+console.log(await makerEntity)
+console.log(await makerEntity.perform())
+
+export default 1
+
+/*
+* Aprendizado:
+
+titulo: "tecnica factoryMethod para criar classe utlitaria podendo usar o this dentro dela e fornecendo um objeto maker para uso fora.",
+    categorias: ["configurador_de_objeto", "classe_utilitaria", "uso_this", "factoryMethod", "instanciarPropriaClasse"],
+    contexto: "factoryMethod criar metodo static preMaker instanciando a classe , e fora criar o maker chamando a classe executando o statico preMaker() -- ou seja o preMaker é criado no metodo statico dentro da classe e ele é usado fora para gerar o maker",
+    conceito: `
+    -- o construtor pode estar vazio, porque vamos usar somente seus metodos fora.
+    -- podemos usar o this para acessar as props da classe, nao precisa criar props staticas e acessar com a classe.
+
+    -- macete : factoryMethod criar metodo static preMaker instanciando a classe , e fora criar o maker chamando a classe executando o statico preMaker()
+    -- ou seja o preMaker é criado no metodo statico dentro da classe e ele é usado fora para gerar o maker.
+    `,
+
+exemplos: `
+
+`,
+
+*/
--- _fundamentos_PGM_semi_estruturado/pgm/paradigma.POO.ts
+++ _fundamentos_PGM_semi_estruturado/pgm/paradigma.POO.ts
@@ -35,7 +35,7 @@
     categorias: ["configurador_de_objeto", "classe_utilitaria", "uso_this"],
     contexto: "o macete é pré instanciar a classe, e depois com a instancia fazer o objetivo ou usar o objeto ou usar o emtodo do objeto.",
     conceito: `
-    -- o construtor pode estar vazio, porque vamosusar somente seus metodos fora.
+    -- o construtor pode estar vazio, porque vamos usar somente seus metodos fora.
     -- podemos usar o this para acessar as props da classe, nao precisa criar props staticas e acessar com a classe.
     -- préUso : instanciamos a classe para poder usar seu objeto pára criar um maker.
     -- guardamos em uma variavel a execucao da préInstancia criando assim um maker.
@@ -63,6 +63,47 @@
     const intanceEntity = async () => await new Entity()
 
     const makerEntity = await intanceEntity()
+
+    console.log(await makerEntity)
+    console.log(await makerEntity.perform())
+
+    export default 1
+
+    `,
+  },
+  {
+    titulo: "tecnica factoryMethod para criar classe utlitaria podendo usar o this dentro dela e fornecendo um objeto maker para uso fora.",
+    categorias: ["configurador_de_objeto", "classe_utilitaria", "uso_this", "factoryMethod", "instanciarPropriaClasse"],
+    contexto: "factoryMethod criar metodo static preMaker instanciando a classe , e fora criar o maker chamando a classe executando o statico preMaker() -- ou seja o preMaker é criado no metodo statico dentro da classe e ele é usado fora para gerar o maker",
+    conceito: `
+    -- o construtor pode estar vazio, porque vamos usar somente seus metodos fora.
+    -- podemos usar o this para acessar as props da classe, nao precisa criar props staticas e acessar com a classe.
+
+    -- macete : factoryMethod criar metodo static preMaker instanciando a classe , e fora criar o maker chamando a classe executando o statico preMaker()
+    -- ou seja o preMaker é criado no metodo statico dentro da classe e ele é usado fora para gerar o maker.
+
+    `,
+    observacoes: "#null",
+    exemplos: `
+    class Entity {
+      private campo1!: number
+      private campo2!: number
+
+      constructor() {
+        this.campo1 = 10
+        this.campo2 = 20
+      }
+
+      static _preMaker() {
+        return new Entity()
+      }
+
+      async perform() {
+        return await \`>>>>>>>>>> Hello Word >> Comput Soma C1+C2 === \${this.campo1 + this.campo2}\`
+      }
+    }
+
+    const makerEntity = await Entity._preMaker()
 
     console.log(await makerEntity)
     console.log(await makerEntity.perform())
