700d1e26e57742f07d290d2627a0fdb1936f094b
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-13 13:52:53 -0300

    feat: escapes em strings

---
+++ _fundamentos_PGM_semi_estruturado/pgm/escapes.ts
@@ -0,0 +1,17 @@
+// deno-lint-ignore-file no-unused-vars
+
+const escapes_em_strings = {
+  titulo: "escapes codigo em strings",
+
+  categorias: ["escapes",],
+
+  tem_que_ser_escapados_com_barra_invertida: [
+    "$",
+    "` antes do inicio  e do final de crazes`",
+  ],
+
+  exemplos: [
+    "\$",
+    "\` antes do inicio  e do final de crazes\`",
+  ],
+}
