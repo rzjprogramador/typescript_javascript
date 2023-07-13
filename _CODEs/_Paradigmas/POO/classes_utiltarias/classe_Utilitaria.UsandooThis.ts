7cc5f7f64726b150d0051e02e8dbbfa7459073eb
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-07-13 12:03:27 -0300

    feat: classe utilitaria usando o this dentro dela

--- _CODEs/_Paradigmas/POO/classes_utiltarias/classe_Utilitaria.UsandooThis.ts
+++ _CODEs/_Paradigmas/POO/classes_utiltarias/classe_Utilitaria.UsandooThis.ts
@@ -7,22 +7,18 @@ class Entity {
     this.campo1 = 10
     this.campo2 = 20
   }
-  // obs: o construtor pode estar vazio, porque vamosusar somente seus metodos fora.
 
   async perform() {
     return await `>>>>>>>>>> Hello Word >> Comput Soma C1+C2 === ${this.campo1 + this.campo2}`
   }
 }
 
-// -- préUso: instanciamos a classe para poder usar seu objeto pára criar um maker.
 const intanceEntity = async () => await new Entity()
 
-// -- guardamos em uma variavel a execucao da préInstancia criando assim um maker
-const makerInstance = await intanceEntity()
+const makerEntity = await intanceEntity()
 
-// -- usamos o maker fora da classe -- tendo acesso as seus membros prototypes.
-console.log(await makerInstance)
-console.log(await makerInstance.perform())
+console.log(await makerEntity)
+console.log(await makerEntity.perform())
 
 export default 1
 
